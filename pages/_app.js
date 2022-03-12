f2a4e41b910431a6411fc911d4bad018e2aa3a88
barbora <47380995+bstrihova@users.noreply.github.com>
2022-03-12 19:50:07 +0100

    add themeprovider to set primary color blue

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 // import layout  components form components folder
 import Layout from "../src/components/layout";
+import { blue } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function MyApp({ Component, pageProps }) {
+  const theme = createTheme({
+    palette: {
+      primary: blue,
+    },
+  });
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
