0a2ffcf3b68ad6920975f460eef8469485021f72
fogelek <michal@fogelman.pl>
2022-03-12 19:01:26 +0100

    smaller header

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,7 +36,14 @@ function Layout({ children }) {
     <>
       <CssBaseline />
       <Container>
-        <AppBar position="static" sx={{ bgcolor: "#0057b7" }} elevation={0}>
+        <AppBar
+          position="static"
+          sx={{
+            bgcolor: "#0057b7",
+            "& .MuiToolbar-root": { padding: "4px 12px" },
+          }}
+          elevation={0}
+        >
           <Toolbar sx={{ padding: 2 }}>
             <img src="/static/img/lindat-logo.svg" style={{ width: "88px" }} />
             <Typography
