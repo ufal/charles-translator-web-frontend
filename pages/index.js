ce2b6586e6a226180bb3cdfc373ddb23604290b6
mfogelman <mfogelman@microsoft.com>
2022-03-12 13:29:20 +0100

    Wide layout Multiline inputs

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,59 @@
-import {TextField} from "@mui/material";
-import styled from 'styled-components'
-
-
-
+import { TextField } from "@mui/material";
+import styled from "styled-components";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const Grid = styled.div`
-    display: grid;
+  display: grid;
   width: 100%;
   height: 100%;
   position: absolute;
-  grid-template-rows: 40px 1fr 1fr;
+  grid-gap: 12px;
+  @media (min-width: 1200px) {
+    grid-template-rows: 40px 1fr;
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (max-width: 1200px) {
+    grid-template-rows: 40px 1fr 1fr;
+  }
 `;
 
 const Header = styled.div`
-width: 100%;
+  width: 100%;
   height: 100%;
   background: cornflowerblue;
+  @media (min-width: 1200px) {
+    grid-column: span 2;
+  }
 `;
 
 const Index = () => (
   <Grid>
-    <Header/>
-      <TextField id="source" label="Outlined" variant="outlined" multiline />
-      <TextField id="destination" label="Outlined" variant="outlined" multiline />
+    <CssBaseline />
+    <Header />
+    <TextField
+      id="source"
+      className="field"
+      label="Outlined"
+      variant="outlined"
+      multiline
+      sx={{
+        "& .MuiOutlinedInput-root": {
+          height: "100%",
+        },
+      }}
+    />
+    <TextField
+      id="destination"
+      className="field"
+      label="Outlined"
+      variant="outlined"
+      multiline
+      sx={{
+        "& .MuiOutlinedInput-root": {
+          height: "100%",
+        },
+      }}
+    />
   </Grid>
 );
 
