f493c71019b48fa4b61dc4b52b7c8819a7ed0530
mfogelman <mfogelman@microsoft.com>
2022-03-12 12:57:29 +0100

    Base layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
+import {TextField} from "@mui/material";
+import styled from 'styled-components'
+
+
+
+
+const Grid = styled.div`
+    display: grid;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  grid-template-rows: 40px 1fr 1fr;
+`;
+
+const Header = styled.div`
+width: 100%;
+  height: 100%;
+  background: cornflowerblue;
+`;
+
 const Index = () => (
-  <div>
-    <h1>Home page</h1>
-  </div>
+  <Grid>
+    <Header/>
+      <TextField id="source" label="Outlined" variant="outlined" multiline />
+      <TextField id="destination" label="Outlined" variant="outlined" multiline />
+  </Grid>
 );
 
 export default Index;
