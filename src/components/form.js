8007219ce99ede0889e71766b1953cdc457c26be
David <d@nogare.cz>
2022-03-25 00:12:35 +0100

    styles cleanUp

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-
+import styled from "styled-components";
+import debounce from "debounce-promise";
 import {
   Box,
   IconButton,
@@ -12,76 +13,18 @@ import {
 } from "@mui/material";
 import {
   Clear as ClearIcon,
+  ErrorOutline as ErrorOutlineIcon,
+  ContentCopy as ContentCopyIcon,
   SwapVert,
 } from "@mui/icons-material";
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import { blue } from "@mui/material/colors";
-
-import styled from "styled-components";
-import debounce from "debounce-promise";
 
 import { TranslationHistory } from "./TranslationHistory";
 import { translate } from "../api";
 import { getHistory, saveHistory } from "../history";
-import {
-  transliterateCyrilToLatin,
-  transliterateLatinToCyril,
-} from "../transliterate";
+import { transliterateCyrilToLatin, transliterateLatinToCyril } from "../transliterate";
 
+import styles from "./form.module.scss"
 
-const Flex = styled.div`
-  display: flex;
-  flex-direction: row;
-  @media (min-width: 769px) {
-    margin: 12px;
-  }
-  @media (max-width: 768px) {
-    flex-direction: column;
-    margin-top: 8px;
-  }
-`;
-
-const SwitchButtonWrapper = styled.div`
-  align-self: center;
-  justify-self: center;
-  @media (min-width: 769px) {
-    transform: rotate(90deg);
-  }
-  margin: 4px;
-`;
-
-const Transliteration = styled.div`
-  color: grey;
-  word-break: break-all;
-  margin-bottom: 0;
-`;
-
-const LabelContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 38px;
-  align-items: center;
-  flex-shrink: 0;
-  @media (max-width: 768px) {
-    padding: 0 8px;
-  }
-`;
-
-const Label = styled.label`
-  font-size: 0.9rem;
-`;
-
-const TranslationFieldContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  @media (min-width: 769px) {
-    width: calc(50% - 20px);
-  }
-  @media (max-width: 768px) {
-    max-width: 100%;
-  }
-`;
 
 const debouncedTranslate = debounce((setLoading, data) => {setLoading ; return translate(data) }, 500);
 const debouncedSave = debounce(saveHistory, 10000);
@@ -145,8 +88,8 @@ const Form = () => {
     .catch((error) => {
       setLoading(false);
       setLoadingError(true);
-      console.error("Error when loading translation");
-      console.error(error);
+      console.log("Error when loading translation");
+      console.log(error);
     })
   }
 
@@ -170,137 +113,133 @@ const Form = () => {
     }
   }, [source, languages]);
 
-  const { query } = useRouter();
-
   return (
-    <>
-      <Flex>
-        <TranslationFieldContainer>
-          <LabelContainer>
-            <Label htmlFor="destination">{languages.source.name}</Label>
-          </LabelContainer>
-          <TextField
-            value={source}
-            onChange={(e) => handleChangeSource(e.target.value)}
-            id="source"
-            variant="filled"
-            multiline
-            minRows={6}
-            sx={{
-              "& .MuiInputBase-root": {
-                paddingTop: "8px",
-                paddingRight: "40px",
-              },
-            }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position='end'>
-                  { source.length !== 0 &&
-                    <Tooltip title="Clear source text">
-                      <IconButton 
-                        onClick={clearSource}
-                        sx={{
-                          position: "absolute",
-                          top: "10px",
-                          right: "10px",
-                        }}
-                      >
-                        <ClearIcon/>
-                      </IconButton>
-                    </Tooltip>
-                  }
-                </InputAdornment>
-              ),
-            }}
+    <div className={styles.flex}>
+      <div className={styles.translationFieldContainer}>
+        <div className={styles.labelContainer}>
+          <label className={styles.label} htmlFor="destination">{languages.source.name}</label>
+        </div>
+        <TextField
+          value={source}
+          onChange={(e) => handleChangeSource(e.target.value)}
+          id="source"
+          variant="filled"
+          multiline
+          minRows={6}
+          sx={{
+            "& .MuiInputBase-root": {
+              paddingTop: "8px",
+              paddingRight: "40px",
+            },
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                { source.length !== 0 &&
+                  <Tooltip title="Clear source text">
+                    <IconButton 
+                      onClick={clearSource}
+                      sx={{
+                        position: "absolute",
+                        top: "10px",
+                        right: "10px",
+                      }}
+                    >
+                      <ClearIcon/>
+                    </IconButton>
+                  </Tooltip>
+                }
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
+
+      <div className={styles.switchButtonWrapper}>
+        <Tooltip title="Swap languages">
+          <IconButton
+            aria-label="switch languages"
+            onClick={flipLanguages}
+            size="large"
+          >
+            <SwapVert fontSize="large" color="primary" />
+          </IconButton>
+        </Tooltip>
+      </div>
+
+      <div className={styles.translationFieldContainer}>
+        <Box
+          sx={{
+            height: "38px",
+            minHeight: "38px",
+            display:"flex",
+            flexDirection: "row",
+            flexWrap: "nowrap",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <label className={styles.label} htmlFor="destination">{languages.target.name}</label>
+          {translation.length !== 0 && navigator.clipboard !== 'undefined' && <Tooltip
+            title="Copy translation to cliboard"
+          >
+            <Button 
+              onClick={() => {navigator.clipboard.writeText(translation)}}
+              variant="text"
+              size="small"
+              startIcon={<ContentCopyIcon/>}
+            >
+              COPY
+            </Button>
+          </Tooltip>}
+          <TranslationHistory
+            getHistory={() => getHistory(languages.source)}
+            onSelect={handleChangeSource}
           />
-        </TranslationFieldContainer>
-
-        <SwitchButtonWrapper>
-          <Tooltip title="Swap languages">
-            <IconButton
-              aria-label="switch languages"
-              onClick={flipLanguages}
-              size="large"
+        </Box>
+        {loading && (<LinearProgress sx={{ top: "4px", marginTop: "-4px" }} />)}
+        <Box
+          padding={2}
+          sx={{
+            backgroundColor: "#e3f2fd",
+            borderRadius: 1,
+            height: "100%",
+            minHeight: "157px",
+            marginBottom: "20px",
+            overflowWrap: "break-word",
+          }}
+        >
+          {loadingError ? 
+            <Box
+              sx={{
+                display: "grid",
+                alignItems: "center",
+                justifyItems: "center",
+                height: "100%",
+                minHeight: "100%",
+              }}
             >
-              <SwapVert fontSize="large" color="primary" />
-            </IconButton>
-          </Tooltip>
-        </SwitchButtonWrapper>
-
-        <TranslationFieldContainer>
-          <Box
-            sx={{
-              height: "38px",
-              minHeight: "38px",
-              display:"flex",
-              flexDirection: "row",
-              flexWrap: "nowrap",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Label htmlFor="destination">{languages.target.name}</Label>
-            {translation.length !== 0 && navigator.clipboard !== 'undefined' && <Tooltip
-              title="Copy translation to cliboard"
-            >
-              <Button 
-                onClick={() => {navigator.clipboard.writeText(translation)}}
-                variant="text"
-                size="small"
-                startIcon={<ContentCopyIcon/>}
-              >
-                COPY
-              </Button>
-            </Tooltip>}
-            <TranslationHistory
-              getHistory={() => getHistory(languages.source)}
-              onSelect={handleChangeSource}
-            />
-          </Box>
-          {loading && (<LinearProgress sx={{ top: "4px", marginTop: "-4px" }} />)}
-          <Box
-            padding={2}
-            sx={{
-              backgroundColor: blue[50],
-              borderRadius: 1,
-              height: "100%",
-              minHeight: "157px",
-              marginBottom: "20px",
-              overflowWrap: "break-word",
-            }}
-          >
-            {loadingError ? 
-              <Box
-                sx={{
-                  display: "grid",
-                  alignItems: "center",
-                  justifyItems: "center",
-                  height: "100%",
-                  minHeight: "100%",
-                }}
-              >
-                <ErrorOutlineIcon/>
-                <span>Translation error</span>
-                <Button
-                  onClick={()=>{location.reload();}}
-                >Try again</Button>
-              </Box>
-              :
+              <ErrorOutlineIcon/>
+              <span>Translation error</span>
+              <Button
+                onClick={()=>{location.reload();}}
+              >Try again</Button>
+            </Box>
+            :
+            <Box>
               <Box>
-                <Box>
-                  <strong>{translation.split('\n').map((item, i) => (<p key={i} style={{margin: 0}}>{(item != "") ? item : <br />}</p>))}</strong>
-                </Box>
-              
-                <Transliteration>
-                  {languages.target.transliterate(translation).split('\n').map((item, i) => (<p key={i} style={{margin: 0}}>{(item != "") ? item : <br />}</p>))}
-                </Transliteration>
+                <strong>{translation.split('\n').map((item, i) => (<p key={i} style={{margin: 0}}>{(item != "") ? item : <br />}</p>))}</strong>
               </Box>
-            }
-          </Box>
             
-        </TranslationFieldContainer>
-      </Flex>
-    </>
+              <div className={styles.transliteration}>
+                {languages.target.transliterate(translation).split('\n').map((item, i) => (<p key={i} style={{margin: 0}}>{(item != "") ? item : <br />}</p>))}
+              </div>
+            </Box>
+          }
+        </Box>
+          
+      </div>
+    </div>
   );
 };
 
