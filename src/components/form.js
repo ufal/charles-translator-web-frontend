dd76438cfe0df5180de691e9f1c989fa0b69bc9b
David <d@nogare.cz>
2022-03-25 01:52:30 +0100

    large code refactory

--- src/components/form.js
+++ src/components/form.js
@@ -1,21 +1,20 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import styled from "styled-components";
 import debounce from "debounce-promise";
 import {
-  Box,
-  IconButton,
-  InputAdornment,
-  LinearProgress,
-  TextField,
-  Tooltip,
-  Button,
+	Box,
+	IconButton,
+	InputAdornment,
+	LinearProgress,
+	TextField,
+	Tooltip,
+	Button,
 } from "@mui/material";
 import {
-  Clear as ClearIcon,
-  ErrorOutline as ErrorOutlineIcon,
-  ContentCopy as ContentCopyIcon,
-  SwapVert,
+	Clear as ClearIcon,
+	ErrorOutline as ErrorOutlineIcon,
+	ContentCopy as ContentCopyIcon,
+	SwapVert,
 } from "@mui/icons-material";
 
 import { TranslationHistory } from "./TranslationHistory";
@@ -30,203 +29,175 @@ const debouncedTranslate = debounce(translate, 500);
 const debouncedSave = debounce(saveHistory, 10000);
 
 const languageUk = {
-  id: "uk",
-  name: "Українською",
-  transliterate: transliterateCyrilToLatin,
+	id: "uk",
+	name: "Українською",
+	transliterate: transliterateCyrilToLatin,
 };
 
 const languageCs = {
-  id: "cs",
-  name: "Česky",
-  transliterate: transliterateLatinToCyril,
+	id: "cs",
+	name: "Česky",
+	transliterate: transliterateLatinToCyril,
 };
 
 let loadingID = 0; // id of most recent sent request
-let loadedID = 0; // id o most recent received request
+let loadedID = 0;  // id o most recent received request
 
 const Form = () => {
-  const [source, setSource] = useState("");
-  const [translation, setTranslation] = useState("");
-  const [languages, setLanguages] = useState({ source: languageUk, target: languageCs });
-  const [loading, setLoading] = useState(false);
-  const [loadingError, setLoadingError] = useState(false);
+	const [source, setSource] = useState("");
+	const [translation, setTranslation] = useState("");
+	const [languages, setLanguages] = useState({ source: languageUk, target: languageCs });
+	const [loading, setLoading] = useState(false);
+	const [loadingError, setLoadingError] = useState(false);
 
-  React.useEffect(() => {
-    const defaultSource = localStorage.getItem("lastTranslationSource") || "";
+	React.useEffect(() => {
+		const defaultSource = localStorage.getItem("lastTranslationSource") || "";
 
-    if(defaultSource === languageCs.id)
-      setLanguages({source: languageCs, target: languageUk });
-    else
-      setLanguages({source: languageUk, target: languageCs });
-  }, [])
+		if(defaultSource === languageCs.id)
+			setLanguages({source: languageCs, target: languageUk });
+		else
+			setLanguages({source: languageUk, target: languageCs });
+	}, [])
 
-  function handleChangeSource(text, fromLanguage = languages.source.id, toLanguage = languages.target.id) {
-    setSource(text);
-    setLoading(true);
+	function handleChangeSource(text, fromLanguage = languages.source.id, toLanguage = languages.target.id) {
+		setSource(text);
+		setLoading(true);
 
-    debouncedSave(languages.source, text);
-    debouncedTranslate({
-      text,
-      fromLanguage,
-      toLanguage,
-      loadingID: ++loadingID,
-    })
-    .then((data) => {
-      // this request is last that was sent
-      if(data.loadingID === loadingID)
-        setLoading(false);
-      
-      // this request has some new information
-      if(loadedID < data.loadingID){
-        loadedID = data.loadingID;
-        setTranslation(data.data.trim());
-        setLoadingError(false);
-      }
+		debouncedSave(languages.source, text);
+		debouncedTranslate({
+			text,
+			fromLanguage,
+			toLanguage,
+			loadingID: ++loadingID,
+		})
+		.then((data) => {
+			// this request is last that was sent
+			if(data.loadingID === loadingID)
+				setLoading(false);
+			
+			// this request has some new information
+			if(loadedID < data.loadingID){
+				loadedID = data.loadingID;
+				setTranslation(data.data.trim());
+				setLoadingError(false);
+			}
 
-    })
-    .catch((error) => {
-      setLoading(false);
-      setLoadingError(true);
-      console.log("Error when loading translation");
-      console.log(error);
-    })
-  }
+		})
+		.catch((error) => {
+			setLoading(false);
+			setLoadingError(true);
+			console.log("Error when loading translation");
+			console.log(error);
+		})
+	}
 
-  const flipLanguages = () => {
-    const oldSource = languages.source;
-    const oldTarget = languages.target;
-    setTranslation("");
-    setLanguages((state) => ({ source: state.target, target: state.source }));
+	const flipLanguages = () => {
+		const oldSource = languages.source;
+		const oldTarget = languages.target;
+		setTranslation("");
+		setLanguages((state) => ({ source: state.target, target: state.source }));
 
-    /**/// switch - keep source text as source
-    handleChangeSource(source, oldTarget.id, oldSource.id);
-    /*/           - insert translation as new source
-    handleChangeSource(translation, oldTarget.id, oldSource.id);
-    /**/
-    
-    if(typeof window !== 'undefined')
-      window.localStorage.setItem("lastTranslationSource", oldTarget.id);
-  }
+		/**/// switch - keep source text as source
+		handleChangeSource(source, oldTarget.id, oldSource.id);
+		/*/                     - insert translation as new source
+		handleChangeSource(translation, oldTarget.id, oldSource.id);
+		/**/
+		
+		if(typeof window !== 'undefined')
+			window.localStorage.setItem("lastTranslationSource", oldTarget.id);
+	}
 
-  return (
-    <div className={styles.flex}>
-      <div className={styles.translationFieldContainer}>
-        <div className={styles.labelContainer}>
-          <label className={styles.label} htmlFor="destination">{languages.source.name}</label>
-        </div>
-        <TextField
-          value={source}
-          onChange={(e) => handleChangeSource(e.target.value)}
-          id="source"
-          variant="filled"
-          multiline
-          minRows={6}
-          className={styles.sourceInput}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position='end'>
-                { source.length !== 0 &&
-                  <Tooltip title="Clear source text">
-                    <IconButton 
-                      onClick={() => {handleChangeSource("")}}
-                      className={styles.removeButton}
-                    >
-                      <ClearIcon/>
-                    </IconButton>
-                  </Tooltip>
-                }
-              </InputAdornment>
-            ),
-          }}
-        />
-      </div>
+	return (
+		<div className={styles.flex}>
+			<div className={styles.translationFieldContainer}>
+				<div className={styles.labelContainer}>
+					<label className={styles.label} htmlFor="destination">{languages.source.name}</label>
+				</div>
+				<TextField
+					value={source}
+					onChange={(e) => handleChangeSource(e.target.value)}
+					id="source"
+					variant="filled"
+					multiline
+					minRows={6}
+					className={styles.sourceInput}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position='end'>
+								{ source.length !== 0 &&
+									<Tooltip title="Clear source text">
+										<IconButton 
+											onClick={() => {handleChangeSource("")}}
+											className={styles.removeButton}
+										>
+											<ClearIcon/>
+										</IconButton>
+									</Tooltip>
+								}
+							</InputAdornment>
+						),
+					}}
+				/>
+			</div>
 
-      <div className={styles.switchButtonWrapper}>
-        <Tooltip title="Swap languages">
-          <IconButton
-            aria-label="switch languages"
-            onClick={flipLanguages}
-            size="large"
-          >
-            <SwapVert fontSize="large" color="primary" />
-          </IconButton>
-        </Tooltip>
-      </div>
+			<div className={styles.switchButtonWrapper}>
+				<Tooltip title="Swap languages">
+					<IconButton
+						aria-label="switch languages"
+						onClick={flipLanguages}
+						size="large"
+					>
+						<SwapVert fontSize="large" color="primary" />
+					</IconButton>
+				</Tooltip>
+			</div>
 
-      <div className={styles.translationFieldContainer}>
-        <Box
-          sx={{
-            height: "38px",
-            minHeight: "38px",
-            display:"flex",
-            flexDirection: "row",
-            flexWrap: "nowrap",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <label className={styles.label} htmlFor="destination">{languages.target.name}</label>
-          {translation.length !== 0 && navigator.clipboard !== 'undefined' && <Tooltip
-            title="Copy translation to cliboard"
-          >
-            <Button 
-              onClick={() => {navigator.clipboard.writeText(translation)}}
-              variant="text"
-              size="small"
-              startIcon={<ContentCopyIcon/>}
-            >
-              COPY
-            </Button>
-          </Tooltip>}
-          <TranslationHistory
-            getHistory={() => getHistory(languages.source)}
-            onSelect={handleChangeSource}
-          />
-        </Box>
-        {loading && (<LinearProgress sx={{ top: "4px", marginTop: "-4px" }} />)}
-        <Box
-          padding={2}
-          sx={{
-            backgroundColor: "#e3f2fd",
-            borderRadius: 1,
-            height: "100%",
-            minHeight: "157px",
-            marginBottom: "20px",
-            overflowWrap: "break-word",
-          }}
-        >
-          {loadingError ? 
-            <Box
-              sx={{
-                display: "grid",
-                alignItems: "center",
-                justifyItems: "center",
-                height: "100%",
-                minHeight: "100%",
-              }}
-            >
-              <ErrorOutlineIcon/>
-              <span>Translation error</span>
-              <Button
-                onClick={()=>{location.reload();}}
-              >Try again</Button>
-            </Box>
-            :
-            <Box>
-              <Box>
-                <strong>{translation.split('\n').map((item, i) => (<p key={i} style={{margin: 0}}>{(item != "") ? item : <br />}</p>))}</strong>
-              </Box>
-            
-              <div className={styles.transliteration}>
-                {languages.target.transliterate(translation).split('\n').map((item, i) => (<p key={i} style={{margin: 0}}>{(item != "") ? item : <br />}</p>))}
-              </div>
-            </Box>
-          }
-        </Box>
-          
-      </div>
-    </div>
-  );
+			<div className={styles.translationFieldContainer}>
+				<div className={styles.translationHeader}>
+					<label className={styles.label} htmlFor="destination">{languages.target.name}</label>
+					{translation.length !== 0 && navigator.clipboard !== 'undefined' &&
+						<Tooltip title="Copy translation to cliboard">
+						<Button 
+							onClick={() => {navigator.clipboard.writeText(translation)}}
+							variant="text"
+							size="small"
+							startIcon={<ContentCopyIcon/>}
+						>
+							COPY
+						</Button>
+					</Tooltip>}
+					<TranslationHistory
+						getHistory={() => getHistory(languages.source)}
+						onSelect={handleChangeSource}
+					/>
+				</div>
+				{loading && (<LinearProgress className={styles.loadingBar}/>)}
+				<div className={styles.translationOutput}>
+					{loadingError ? 
+						<div className={styles.networkError}>
+							<ErrorOutlineIcon/>
+							<span>Translation error</span>
+							<Button
+								onClick={()=>{location.reload();}}
+							>
+								Try again
+							</Button>
+						</div>
+						:
+						<div>
+							<div className={styles.translationText}>
+								{translation.split('\n').map((item, i) => (<p key={i} style={{margin: 0}}>{(item !== "") ? item : <br />}</p>))}
+							</div>
+						
+							<div className={styles.transliteration}>
+								{languages.target.transliterate(translation).split('\n').map((item, i) => (<p key={i} style={{margin: 0}}>{(item !== "") ? item : <br />}</p>))}
+							</div>
+						</div>
+					}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Form;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,180 +1,142 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import {
-  Typography,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Button,
-  Tooltip,
-  Dialog,
-  Snackbar,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  DialogContentText,
+	Typography,
+	AppBar,
+	Toolbar,
+	IconButton,
+	Button,
+	Tooltip,
+	Dialog,
+	Snackbar,
+	DialogTitle,
+	DialogActions,
 } from "@mui/material";
 import { Info as InfoIcon } from "@mui/icons-material";
 import CloseIcon from '@mui/icons-material/Close';
 import CheckIcon from '@mui/icons-material/Check';
-import { blue } from "@mui/material/colors";
 
 import AboutUs from "./about-us";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  min-height: 100%;
-  position: absolute;
-`;
+import styles from "./layout.module.scss"
 
-const FlagsContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-const HomeLink = styled.a`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-right: auto;
-  cursor: pointer;
-`;
-
-const Footer = styled.footer`
-  margin: auto 0 0;
-  padding: 8px;
-  font-size: 0.7rem;
-  text-align: center;
-  background-color: ${blue[100]};
-  color: ${blue[900]};
-  @media (max-width: 768px) {
-    font-size: 0.55rem;
-  }
-`;
 
 function Layout({ children }) {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [collectionSnackbar, setCollectionSnackbar] = useState(true);
-  const [openAboutUs, setOpenAboutUs] = React.useState(false);
-  const [forOrganizations, setForOrganizations] = React.useState(false);
+	const [collectionSnackbar, setCollectionSnackbar] = useState(true);
+	const [openAboutUs, setOpenAboutUs] = useState(false);
+	const [forOrganizations, setForOrganizations] = useState(false);
 
-  React.useEffect(() => setCollectionSnackbar(localStorage.getItem("collectDataConsentValue") !== "true"),[])
-  React.useEffect(() => setForOrganizations((localStorage.getItem("organizationName") || "").length !== 0))
+	useEffect(() => setCollectionSnackbar(localStorage.getItem("collectDataConsentValue") !== "true"),[])
+	useEffect(() => setForOrganizations((localStorage.getItem("organizationName") || "").length !== 0))
 
-  const allowCollection = () => { 
-    setCollectionSnackbar(false);
-    if(typeof window !== 'undefined'){
-      window.localStorage.setItem(
-        "collectDataConsentValue",
-        "true"
-      );
-    }
-  }
+	const allowCollection = () => { 
+		setCollectionSnackbar(false);
+		if(typeof window !== 'undefined')
+			window.localStorage.setItem("collectDataConsentValue", "true");
+	}
 
-  return (
-    <>
-      <CssBaseline />
-      <Container>
-        <AppBar
-          position="static"
-          sx={{
-            bgcolor: blue,
-            "& .MuiToolbar-root": { padding: "4px 0 4px 12px" },
-          }}
-          elevation={0}
-        >
-          <Toolbar sx={{ padding: 2 }}>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, ml: 1 }}
-              color="white"
-            >
-              <FlagsContainer>
-                <img
-                  src="/static/img/ukraine.png"
-                  style={{ width: "30px", marginRight: "10px" }}
-                />
-                <img
-                  src="/static/img/czech-republic.png"
-                  style={{ width: "30px", marginRight: "10px" }}
-                />
-                <p>ÚFAL Translator {forOrganizations && "for organizations"}</p>
-              </FlagsContainer>
-            </Typography>
-            <Tooltip title="About us">
-              <IconButton
-                size="small"
-                edge="start"
-                aria-label="menu"
-                sx={{ mr: 1, color: "white" }}
-                onClick={() => setOpenAboutUs(true)}
-              >
-                <InfoIcon />
-              </IconButton>
-            </Tooltip>
-          </Toolbar>
-        </AppBar>
+	return (
+		<>
+			<CssBaseline />
+			<div className={styles.container}>
+				<AppBar
+					position="static"
+					sx={{
+						bgcolor: "#2196f3",
+						"& .MuiToolbar-root": { padding: "4px 0 4px 12px" },
+					}}
+					elevation={0}
+				>
+					<Toolbar sx={{ padding: 2 }}>
+						<Typography
+							variant="h6"
+							component="div"
+							sx={{ flexGrow: 1, ml: 1 }}
+							color="white"
+						>
+							<div className={styles.flagsContainer}>
+								<img
+									src="/static/img/ukraine.png"
+									className={styles.flagIcon}
+								/>
+								<img
+									src="/static/img/czech-republic.png"
+									className={styles.flagIcon}
+								/>
+								<p>ÚFAL Translator {forOrganizations && "for organizations"}</p>
+							</div>
+						</Typography>
+						<Tooltip title="About us">
+							<IconButton
+								size="small"
+								edge="start"
+								aria-label="menu"
+								sx={{ mr: 1, color: "white" }}
+								onClick={() => setOpenAboutUs(true)}
+							>
+								<InfoIcon />
+							</IconButton>
+						</Tooltip>
+					</Toolbar>
+				</AppBar>
 
-        {children}
+				{children}
 
-        <Dialog
-          PaperProps = {{
-            sx:{maxWidth: "800px"}
-          }}
-          open={openAboutUs}
-          onClose={()=>setOpenAboutUs(false)}
-        >
-          <DialogTitle>
-            <IconButton
-              sx={{
-                position: 'absolute',
-                right: 8,
-                top: 8,
-                color: (theme) => theme.palette.grey[500],
-              }}
-              onClick={()=>setOpenAboutUs(false)}
-            >
-              <CloseIcon />
-            </IconButton>
-          </DialogTitle>
-          <AboutUs/>
-          <DialogActions>
-            <Button onClick={()=>setOpenAboutUs(false)}>Close</Button>
-          </DialogActions>
-        </Dialog>
+				<Dialog
+					PaperProps = {{
+						sx:{maxWidth: "800px"}
+					}}
+					open={openAboutUs}
+					onClose={()=>setOpenAboutUs(false)}
+				>
+					<DialogTitle>
+						<IconButton
+							sx={{
+								position: 'absolute',
+								right: 8,
+								top: 8,
+								color: (theme) => theme.palette.grey[500],
+							}}
+							onClick={()=>setOpenAboutUs(false)}
+						>
+							<CloseIcon />
+						</IconButton>
+					</DialogTitle>
+					<AboutUs/>
+					<DialogActions>
+						<Button onClick={()=>setOpenAboutUs(false)}>Close</Button>
+					</DialogActions>
+				</Dialog>
 
-        <Snackbar
-          open={collectionSnackbar}
-          message={`Souhlasím s tím, aby Ústav formální a aplikované lingvistiky
-            MFF UK ukládal vstupy a výstupy z překladače. V případě souhlasu,
-            mohou být anonymizované texty využity pro další vývoj systému.`}
-          anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
-          action={(
-            <React.Fragment>
-              <Button size="large" onClick={allowCollection}>
-                <CheckIcon fontSize="small" />
-                SOUHLASÍM
-              </Button>
-              <Button size="large" onClick={() => setCollectionSnackbar(false)}>
-                <CloseIcon fontSize="small" />
-                NESOUHLASÍM
-              </Button>
-            </React.Fragment>
-          )}
-        />
+				<Snackbar
+					open={collectionSnackbar}
+					message={`Souhlasím s tím, aby Ústav formální a aplikované lingvistiky
+						MFF UK ukládal vstupy a výstupy z překladače. V případě souhlasu,
+						mohou být anonymizované texty využity pro další vývoj systému.`}
+					anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+					action={(
+						<React.Fragment>
+							<Button size="large" onClick={allowCollection}>
+								<CheckIcon fontSize="small" />
+								SOUHLASÍM
+							</Button>
+							<Button size="large" onClick={() => setCollectionSnackbar(false)}>
+								<CloseIcon fontSize="small" />
+								NESOUHLASÍM
+							</Button>
+						</React.Fragment>
+					)}
+				/>
 
-        <Footer>
-          THE LINDAT/CLARIAH-CZ PROJECT (LM2018101; formerly LM2010013,
-          LM2015071) IS FULLY SUPPORTED BY THE MINISTRY OF EDUCATION, SPORTS AND
-          YOUTH OF THE CZECH REPUBLIC UNDER THE PROGRAMME LM OF LARGE
-          INFRASTRUCTURES
-        </Footer>
-      </Container>
-    </>
-  );
+				<div className={styles.footer}>
+					THE LINDAT/CLARIAH-CZ PROJECT (LM2018101; formerly
+					LM2010013, LM2015071) IS FULLY SUPPORTED BY THE MINISTRY OF
+					EDUCATION, SPORTS AND YOUTH OF THE CZECH REPUBLIC UNDER THE
+					PROGRAMME LM OF LARGE INFRASTRUCTURES
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Layout;
