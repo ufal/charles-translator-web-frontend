d7ce390cf6d4a11b2426733cde9722d1b290b101
Jiří Mayer <jirkamayer97@gmail.com>
2023-11-23 17:03:00 +0100

    Formatted root source files

--- src/Application.jsx
+++ src/Application.jsx
@@ -1,30 +1,30 @@
-import Layout from './components/layout';
-import Form from './components/form';
-import { createTheme, ThemeProvider } from '@mui/material/styles'
+import Layout from "./components/layout";
+import Form from "./components/form";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 /*
-    THEME COLORS
-    ------------
+  THEME COLORS
+  ------------
 
-    #f1f2f2 - Appbar Gray
-    #ff1c3f - Red logo text color (the primary color)
-    #d60000 - "This version is not public" bar red color (danger color)
+  #f1f2f2 - Appbar Gray
+  #ff1c3f - Red logo text color (the primary color)
+  #d60000 - "This version is not public" bar red color (danger color)
 */
 
 export function Application() {
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: '#ff1c3f'
-            }
-        },
-    });
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#ff1c3f",
+      },
+    },
+  });
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Layout>
-                <Form />
-            </Layout>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Form />
+      </Layout>
+    </ThemeProvider>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { Application } from "./Application";
 
-import '../i18n'
+import "../i18n";
 
 const appElement = document.getElementById("app");
 const root = createRoot(appElement);
