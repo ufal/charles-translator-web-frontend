2af051332de7937cf9a487b7769ebaf41a390298
Jirka Mayer <jirkamayer97@gmail.com>
2024-06-18 17:43:52 -0400

    Configured Joy UI to work

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,18 +1,30 @@
 import { StrictMode } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { theme } from "./theme";
+import {
+  experimental_extendTheme as materialExtendTheme,
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  THEME_ID as MATERIAL_THEME_ID,
+} from "@mui/material/styles";
+import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
+
+// NOTE: Using both MUI Material and MUI Joy libraries:
+// https://mui.com/joy-ui/integrations/material-ui/
+
+const materialTheme = materialExtendTheme(theme);
 
 export function Application() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      
-      <StrictMode>
-        <RouterProvider router={router} />
-      </StrictMode>
-    </ThemeProvider>
+    <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
+      <JoyCssVarsProvider>
+        <CssBaseline enableColorScheme />
+
+        <StrictMode>
+          <RouterProvider router={router} />
+        </StrictMode>
+      </JoyCssVarsProvider>
+    </MaterialCssVarsProvider>
   );
 }
